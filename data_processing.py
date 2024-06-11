972e02350dc19c3268dd7922d64c2a6d47cc6d0c
Aabha J <aabhaej2018@gmail.com>
2024-06-11 16:16:46 -0400

    Created json processing function

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import nltk
+import json
 #nltk.download('punkt')  # uncomment this if it's not downloaded, comment it out again after downloading
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
@@ -8,16 +9,40 @@ def tokenize(text):
     return nltk.word_tokenize(text)
 
 def stem(word):
+    # Lowercases the word and stemming
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_words, all_words):
     pass
 
-if __name__ == "__main__":
-    x = "Hello, I am a robot programmed to destroy"
-    print(x)
-    print(tokenize(x))
+def get_json_data():
+    with open("intents.json", "r") as f:
+        data = json.load(f)
 
-    test_words = ["organize", "organizes", "organizing"]
-    stemmed_words = [stem(word) for word in test_words]
-    print(stemmed_words)
+
+    all_words = []
+    tags = []
+    xy = []
+    for intent in data["intents"]:
+        tag = intent["tag"]
+        tags.append(tag)
+
+        # 1. Tokenization
+        for pattern in intent["patterns"]:
+            w = tokenize(pattern)
+            all_words.extend(w) # extend adds all the elements of w to all_words
+            xy.append((w, tag))
+
+    ignore_words = ["?", "!", ".", ","]
+
+
+    #2. Lowercasing (done in stemming function) and 3. Stemming and 
+    all_words = [stem(w) for w in all_words if w not in ignore_words] #4. Remove Punctuation
+    all_words = sorted(set(all_words))
+    tags = sorted(set(tags))
+
+    # print(len(xy), "patterns")
+    # print(len(tags), "tags:", tags)
+    # print(len(all_words), "unique stemmed words:", all_words)
+
+    return all_words, tag, xy
