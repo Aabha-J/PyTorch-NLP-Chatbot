de30988750528c50e2ddf7276de9911d47ed7b45
Aabha J <aabhaej2018@gmail.com>
2024-06-11 15:57:02 -0400

    set up tokenizer and stemmer

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,23 @@
 import nltk
-print("hello")
+#nltk.download('punkt')  # uncomment this if it's not downloaded, comment it out again after downloading
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+
+def tokenize(text):
+    return nltk.word_tokenize(text)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_words, all_words):
+    pass
+
+if __name__ == "__main__":
+    x = "Hello, I am a robot programmed to destroy"
+    print(x)
+    print(tokenize(x))
+
+    test_words = ["organize", "organizes", "organizing"]
+    stemmed_words = [stem(word) for word in test_words]
+    print(stemmed_words)
